ea4a3b7a9a49ebe6a10b76ccef138682a57adf5b
Lenin Jimenez Torres <jlenin2527@gmail.com>
2023-04-05 02:10:38 -0500

    gh pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import {Route, Routes, HashRouter  } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import {Home,Developer,Donations} from "../pages/index"
 
 function Router() {
   return (
-    <HashRouter>
-      <Routes>
-          <Route exact element={<Home/>} path="/"/>
-          <Route exact element={<Donations/>} path="/donations"/>
-          <Route exact element={<Developer/>} path="/developer"/>
-      </Routes>
-    </HashRouter>
+    <Routes>
+        <Route exact element={<Home/>} path="/"/>
+        <Route exact element={<Donations/>} path="/donations"/>
+        <Route exact element={<Developer/>} path="/developer"/>
+    </Routes>
 
   )
 }
